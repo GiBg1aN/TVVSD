ae24b452909f9c6d5bef156968894dbb6ee4e4ba
GiBg1aN <bigagliagianluca@gmail.com>
2019-03-29 21:48:16 +0100

    Change learning approach
    Now the verb is passed as input during the disambiguation and it is used
    as a key to filter only to some vectors.

--- visual_verb_disambiguation.py
+++ visual_verb_disambiguation.py
@@ -13,31 +13,28 @@ def filter_image_name(img_name):
 
 
 def simple_disambiguation(images, senses, labels):
-    verb_accuracy = [0, 0]
-    sense_accuracy = [0, 0]
+    accuracy = [0, 0]
     for i in range(len(images)):
         i_t = np.array(images.iloc[i]['caption'])
-        dot_product = senses['definition'].apply(lambda s_t: np.dot(i_t, s_t)).to_numpy()
-        s_hat = np.argmax(dot_product)
-        pred_lemma = senses.iloc[s_hat]['lemma']
-        pred_sense_id = senses.iloc[s_hat]['sense_num']
-        lemma = list(pd.Series(labels.loc[images.iloc[i].name]['lemma']))
-        sense_id = labels.loc[images.iloc[i].name]['sense_chosen']
+        image_id = images.iloc[i].name
+        verbs = labels.query('image == @image_id')["lemma"]
 
-        if pred_lemma in lemma:
-            verb_accuracy[1] += 1
+        for j in range(len(verbs)):
+            verb = verbs.iloc[j]
+            filtered_senses = senses.query("lemma == @verb")
+            dot_product = filtered_senses['definition'].apply(lambda s_t: np.dot(i_t, s_t)).to_numpy()
+            s_hat = np.argmax(dot_product)
+            pred_sense_id = filtered_senses.iloc[s_hat]['sense_num']
+            sense_id = labels.query("image == @image_id and lemma == @verb")['sense_chosen'].iloc[0]
+
             if sense_id == pred_sense_id:
-                sense_accuracy[1] += 1
+                accuracy[1] += 1
             else:
-                sense_accuracy[0] += 1
-        else:
-            verb_accuracy[0] += 1
+                accuracy[0] += 1
 
-    verb_accuracy = verb_accuracy[1] / (verb_accuracy[0] + verb_accuracy[1]) * 100
-    sense_accuracy = sense_accuracy[1] / (sense_accuracy[0] + sense_accuracy[1]) * 100
+    accuracy = accuracy[1] / (accuracy[0] + accuracy[1]) * 100
 
-    print("Verb accuracy is: %s" % verb_accuracy)
-    print("Sense accuracy is: %s" % sense_accuracy)
+    print("Sense accuracy is: %s" % accuracy)
         
 
 def main():
@@ -46,12 +43,11 @@ def main():
     captions_sense_labels = pd.read_csv("full_sense_annotations.csv")
     captions_sense_labels = captions_sense_labels[captions_sense_labels['COCO/TUHOI'] == 'COCO']
     captions_sense_labels['image'] = captions_sense_labels['image'].apply(filter_image_name)
-    captions_sense_labels = captions_sense_labels.set_index(captions_sense_labels['image'])
+
     simple_disambiguation(embedded_captions, embedded_senses, captions_sense_labels)
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     main()
 
-    
 
