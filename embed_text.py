Improve embedding and fix normalisation bug
Now when a word is not known by the word2vec NN, the full sentence
is passed by only discarding that word. Previously the full sentence
was discarded. By doing that, there is more data available for the
embedding. Moreover, a bug have been fixed, since the mean vector
was not normalised, the dot product was not considered as a real
cosine similarity.

embed_text.py
@@ -1,15 +1,10 @@
 import gensim.downloader as api
 import numpy as np
 import pandas as pd
-
 from gensim.utils import simple_preprocess
 from nltk.corpus import stopwords
 from stop_words import get_stop_words
 
-
-print("Loading Network...")
-model = api.load("word2vec-google-news-300")
-model.init_sims(replace=True)
 
 def embed_data_descriptions(model):
     stop_words = list(get_stop_words('en'))
@@ -22,22 +17,16 @@ def embed_data_descriptions(model):
     print("Dataset embedding...")
     for i in range(len(descriptions_df)):
         caption = descriptions_df.iloc[i]['caption']
-        caption_tokens = simple_preprocess(caption)
-        caption_tokens = [w for w in caption_tokens if not w in stop_words]
-
-        use_row = True
-        for token in caption_tokens:
-            if token not in model.vocab:
-                use_row = False
+        caption_tokens = [w for w in simple_preprocess(caption) if not w in stop_words]
+        caption_tokens = [token for token in caption_tokens if token in model.vocab]
         
-        if use_row:
-            acc = np.zeros(model.vector_size)
-            for token in caption_tokens:
-                acc += model.wv.word_vec(token, use_norm=True)
+        acc = np.zeros(model.vector_size)
+        for token in caption_tokens:
+            acc += model.wv.word_vec(token, use_norm=True)
+        word_mean = acc / len(caption_tokens)
+        word_mean /= np.linalg.norm(word_mean)
 
-            descriptions_df.at[i, 'caption'] = acc
-        else:
-            descriptions_df.at[i, 'caption'] = None
+        descriptions_df.iat[i, 1] = word_mean
 
     embedded_descriptions_df = descriptions_df.dropna()
     rows_per_image = pd.DataFrame(embedded_descriptions_df.groupby('image_id')['caption'].count())
@@ -64,30 +53,17 @@ def embed_data_senses(model):
         definition = senses_df.iloc[i]['definition']
         examples = senses_df.iloc[i]['ontonotes_sense_examples']
         examples.replace('\n', ' ')
-        examples_tokens = simple_preprocess(examples)
-        examples_tokens = [w for w in examples_tokens if not w in stop_words]
 
-        use_examples = True
-        for token in examples_tokens:
-            if token not in model.vocab:
-                use_examples = False
-
-        if use_examples:
-            tokens = simple_preprocess(definition + ' ' + examples)
-        else:
-            tokens = simple_preprocess(definition)
-        tokens = [w for w in tokens if not w in stop_words]
+        tokens = [w for w in simple_preprocess(definition + ' ' + examples) if not w in stop_words]
+        tokens = [token for token in tokens if token in model.vocab]
         
         acc = np.zeros(model.vector_size)
-        try:
-            for token in tokens:
-                acc += model.wv.word_vec(token, use_norm=True)
-            word_mean = acc / len(tokens)
+        for token in tokens:
+            acc += model.wv.word_vec(token, use_norm=True)
+        word_mean = acc / len(tokens)
+        word_mean /= np.linalg.norm(word_mean)
 
-            senses_df.iat[i, 2] = word_mean
-        except (KeyError):
-            print("Word: '%s' not found" % token)
-            senses_df.iat[i, 2] = None
+        senses_df.iat[i, 2] = word_mean
 
     senses_df = senses_df.drop(["ontonotes_sense_examples", "visualness_label"], axis=1)
     senses_df = senses_df.dropna()
@@ -98,3 +74,16 @@ def embed_data_senses(model):
     print("Writing completed")
 
 
+def main():
+    # print("Loading Network...")
+    # model = api.load("word2vec-google-news-300")
+    # model.init_sims(replace=True)
+    
+    print("DESCRIPTIONS")
+    embed_data_descriptions(model)
+    print("SENSES")
+    embed_data_senses(model)
+
+
+if __name__ == '__main__':
+    main()