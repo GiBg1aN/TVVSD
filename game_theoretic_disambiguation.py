313d85fc2cdf3f1342caaf11e235218f494e6149
GiBg1aN <bigagliagianluca@gmail.com>
2019-04-06 13:33:41 +0200

    Performance optimisation
    In order to speed up the computations some Pandas dataframe indexes have
    been rebuilt before iterations and for loops have been replaced with:
    enumerate/iterloops. Where possible, vectorised operations have been
    used. Switching from 10s/it to 2.3it/s.

--- game_theoretic_disambiguation.py
+++ game_theoretic_disambiguation.py
@@ -44,8 +44,7 @@ def remove_duplicates(images, sense_labels):
         dups.append(duplicates.iloc[i].name)
     images = images.drop(dups)
 
-    for i in range(len(dups)):
-        dup = dups[i]
+    for dup in dups:
         sense_labels = sense_labels.drop(list(sense_labels.query('image == @dup').index))
 
     return (images, sense_labels)
@@ -101,31 +100,47 @@ def strategy_space(images, senses, sense_labels):
         image_id = images.iloc[i].name
         verb = sense_labels.query('image == @image_id')['lemma'].iloc[0]
         filtered_senses = senses.query('lemma == @verb')
-        m = len(filtered_senses)  # Number of senses for that verb
 
         for j in range(len(filtered_senses)):
             col = filtered_senses.iloc[j].name  # Columns: index of sense in pandas dataframe
-            S[i, col] = 1 / m
+            S[i, col] = 1 / len(filtered_senses)
     return S
 
 
-def strategy_payoff(image_details, W, S, Z, images, senses, sense_labels):
+def sense_indexes_lookup(image_id, senses, sense_labels):
     """
-    Updates strategy space using Replicator Dynamics.
+    Lookup sense indexes in 'senses' for 'image_id'
 
     Args:
-        image_details: dictionary structure containing: image, senses
-            indexes, and image absolute location
+        image_i: input image unique index identifier
+        senses: A dataframe of senses representations
+        sense_labels: A dataframe that contains the verb and the correct
+            sense for each image (indexed using 'image_id')
+
+    Returns:
+        A plain list of integer indexes for the strategy space
+    """
+    verb = sense_labels.at[image_id, 'lemma']
+    verb_sense_idxs = list(senses.query('lemma == @verb').index)
+    return verb_sense_idxs
+
+
+def strategy_payoff(image_details, W, S, Z, images):
+    """
+    Play a game with each player, updating the mixed strategy of the
+    input player using Replicator Dynamics.
+
+    Args:
+        image_details: dictionary structure containing:
+            senses indexes, and image absolute location
         W: Affinity matrix of size nxn
         S: Strategy space of size nxc
         Z: Payoff matrix of size cxc
         images: A dataframe of image representations of size n
-        senses: A dataframe of senses representations of size c
-        sense_labels: A dataframe that contains the verb and the correct
-            sense for each image
 
     Returns:
-        An nxc matrix containing row-wise probability distributions
+        An mx1 vector (where m is the number of senses of such verb)
+        containing probability distributions of input image senses
     """
     pos_i = image_details.get('pos')
     verb_i_sense_idxs = image_details.get('sense_idxs')
@@ -134,11 +149,9 @@ def strategy_payoff(image_details, W, S, Z, images, senses, sense_labels):
     Ax_sum = np.zeros((len(verb_i_sense_idxs), 1))
 
     denominator = 0
-    for j in range(len(images)):
-        pos_j = j
+    for pos_j, row in enumerate(images.itertuples()):
         if pos_i != pos_j:
-            verb_j = sense_labels.at[images.iloc[j].name, 'lemma']
-            verb_j_sense_idxs = list(senses.query('lemma == @verb_j').index)
+            verb_j_sense_idxs = images.at[row.Index, 'sense_indexes']
 
             w_ij = W[pos_i, pos_j]  # Scalar
             Z_ij = Z[verb_i_sense_idxs][:, verb_j_sense_idxs]  # m_i x m_j matrix
@@ -148,8 +161,7 @@ def strategy_payoff(image_details, W, S, Z, images, senses, sense_labels):
             Ax_sum += Ax  # Fraction numerator (sum wZx) i.e. m_j x 1 vector
             denominator += x_i.T @ Ax  # Scalar
 
-    for h in range(len(x_i)):
-        x_i[h] = (x_i[h] * Ax_sum[h] / denominator)[0, 0]
+    x_i = x_i * Ax_sum / denominator
     return x_i
 
 
@@ -163,10 +175,14 @@ def main():
 
     images, sense_labels = remove_duplicates(images, sense_labels)
 
-    # For performance speed up, the index is rebuilt once in order to use index-based access
-    # instead of query access that is computational expensive. In this case image is set as index
+    # For performance speed up, labels the index is rebuilt once in order to use index-based access
+    # instead of query access that is computational expensive. In this case image is set as index.
+    #
+    # For the same reason, sense_indexes for each image are computed once before looping in order
+    # to avoid 'query' usage at each iteration.
     sense_labels.set_index('image', inplace=True)
-
+    images['sense_indexes'] = images.apply(
+        lambda x: sense_indexes_lookup(x.name, senses, sense_labels), axis=1)
 
     # W = affinity_matrix(images['caption'])  # Affinity matrix
     W = np.load('affinity.npy')  # Affinity matrix
@@ -178,27 +194,22 @@ def main():
     Z = np.load('payoff.npy')  # Payoff matrix
 
     accuracy = [0, 0]
-    for i in tqdm(range(len(images))):
-        image_i = images.iloc[i]
-
-        verb_i = sense_labels.at[image_i.name, 'lemma']
-        verb_i_sense_idxs = list(senses.query('lemma == @verb_i').index)
-        pos_i = images.index.get_loc(image_i.name)
-
+    for i, row in tqdm(enumerate(images.itertuples())):
+        verb_i_sense_idxs = images.at[row.Index, 'sense_indexes']
         # In order to reduce the number of calls to 'query' method, gathered data is passed to
         # function as dictionary.
-        image_details = {'image': image_i, 'sense_idxs': verb_i_sense_idxs, 'pos': pos_i}
+        image_details = {'sense_idxs': verb_i_sense_idxs, 'pos': i}
 
-        x_t = np.vstack(S[pos_i, verb_i_sense_idxs])
-        x_t1 = strategy_payoff(image_details, W, S, Z, images, senses, sense_labels)
+        x_t = np.vstack(S[i, verb_i_sense_idxs])
+        x_t1 = strategy_payoff(image_details, W, S, Z, images)
 
         while np.all(np.abs(x_t1 - x_t) > 0.00001):
             x_t = x_t1
-            x_t1 = strategy_payoff(image_details, W, S, Z, images, senses, sense_labels)
+            x_t1 = strategy_payoff(image_details, W, S, Z, images)
             S[i, verb_i_sense_idxs] = x_t1.flatten()
 
         pred_sense_id = senses.iloc[np.argmax(S[i])]['sense_num']
-        sense_id = sense_labels.at[image_i.name, 'sense_chosen']
+        sense_id = sense_labels.at[row.Index, 'sense_chosen']
 
         if sense_id == pred_sense_id:
             accuracy[1] += 1
@@ -208,6 +219,7 @@ def main():
     accuracy = (accuracy[1] / (accuracy[0] + accuracy[1])) * 100
 
     print('Sense accuracy is: %s' % accuracy)
+    np.save('updated_game_space.npy', S)
 
 
 if __name__ == '__main__':
